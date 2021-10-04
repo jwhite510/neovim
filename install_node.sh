45da8711c27c8ebfb9e0dc74a17037ef88318797
jonathon white <jonathonwhite@protonmail.com>
2021-10-04 12:06:13 -0400

    install node with nvm

--- install_node.sh
+++ install_node.sh
@@ -1,3 +1,11 @@
 # Using Ubuntu
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+## list the available versions to install
+# npm list-remote
+
+# nvm install v6.14
