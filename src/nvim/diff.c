12804972b7c1da2f56ae55955947275f0b72af2c
Jonathon <32371757+jwhite510@users.noreply.github.com>
2022-01-05 17:51:12 -0500

    Update src/nvim/diff.c
    Co-authored-by: zeertzjq <zeertzjq@outlook.com>

--- src/nvim/diff.c
+++ src/nvim/diff.c
@@ -2071,7 +2071,9 @@ int ***allocate_comparison_mem(df_iterators_T df_iterators, diff_T *dp)
   return comparison_mem;
 }
 
-void diff_allign_extraction(df_iterators_T df_iterators, diff_T* dp, int df_path_index2, int* decision2) {
+void diff_allign_extraction(df_iterators_T df_iterators, diff_T *dp, int df_path_index2,
+                            int *decision2)
+{
   int *pointers = xmalloc(sizeof(int) * df_iterators.n);
   for (int i = 0; i < df_iterators.n; i++) {
     pointers[i] = 0;
