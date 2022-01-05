25f6101fbac19f0193404911702ca93a75b5aced
Jonathon <32371757+jwhite510@users.noreply.github.com>
2022-01-05 17:32:41 -0500

    Update src/nvim/diff.c
    Co-authored-by: zeertzjq <zeertzjq@outlook.com>

--- src/nvim/diff.c
+++ src/nvim/diff.c
@@ -2216,14 +2216,10 @@ void linematch_nbuffers(diff_T * dp)
   // int b1 = dp->df_valid_buffers[1];
   // int b2 = dp->df_valid_buffers[2];
 
-  int memsize = 1, i = 0, memsize_decisions = 0;
-  while (i < dp->df_valid_buffers_max) {
-    memsize *= (
-        i == 0 ? 2 :
-          ( dp->df_count[dp->df_valid_buffers[i]]  + 1)
-        );
+  int memsize = 1, memsize_decisions = 0;
+  for (int i = 0; i < dp->df_valid_buffers_max; i++) {
+    memsize *= (i == 0 ? 2 : (dp->df_count[dp->df_valid_buffers[i]] + 1));
     memsize_decisions += dp->df_count[dp->df_valid_buffers[i]];
-    i++;
   }
 
   // create the flattened path matrix
