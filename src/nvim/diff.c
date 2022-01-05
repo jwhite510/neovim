7964d4100432e0827e518934e75812d723560a46
Jonathon <32371757+jwhite510@users.noreply.github.com>
2022-01-05 17:52:18 -0500

    Update src/nvim/diff.c
    Co-authored-by: zeertzjq <zeertzjq@outlook.com>

--- src/nvim/diff.c
+++ src/nvim/diff.c
@@ -1850,9 +1850,7 @@ long count_matched_chars_f(char_u** stringps, int* fromValues, int n, int*** com
         int i1 = fromValues[i]; // index of where to get the buffer
         int j1 = fromValues[j];
         if (comparison_mem[pointerindex][i1][j1] == -1) {
-          comparison_mem[pointerindex][i1][j1] = count_matched_chars(
-              stringps[i], stringps[j]
-              );
+          comparison_mem[pointerindex][i1][j1] = count_matched_chars(stringps[i], stringps[j]);
         }
         matched ++;
         matched_chars += comparison_mem[pointerindex][i1][j1];
