a41dc2c45b77e2a2f609d0a37c9b6763db40cc4f
jonathon <jonathonwhite@protonmail.com>
2022-02-19 23:04:39 -0500

    updated debug script

--- debug.sh
+++ debug.sh
@@ -5,10 +5,13 @@ then
   gdb \
       -ex "b screen.c:2338"\
       -ex "r"\
-      --args ./nvim -d ../../testdiffb.txt ../../testdiffa.txt
+      --args ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
+      # --args ./nvim -d ../../testdiffb.txt ../../testdiffa.txt
+      # -ex "b screen.c:2338"\
+      # -ex "b diff.c:free_comparison_mem"\
       # -ex "b diff.c:try_possible_paths"\
 else
-  ./nvim -d ../../testdiffb.txt ../../testdiffa.txt
+  ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
 
 fi
 # gdb \
