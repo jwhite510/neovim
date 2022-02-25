958246898c070d7aa768fbb74af8f2bf3903fa26
jonathon <jonathonwhite@protonmail.com>
2022-02-24 23:13:29 -0500

    update test / debug files

--- debug.sh
+++ debug.sh
@@ -4,14 +4,14 @@ if [ "$1" = "gdb" ]
 then
   gdb \
       -ex "b diff.c:2738"\
-      --args ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
-      # --args ./nvim -u NONE -d ../../testdiffb.txt ../../testdiffa.txt
+      --args ./nvim -u NONE -d ../../testdiffb.txt ../../testdiffa.txt
+      # --args ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
       # -ex "b screen.c:2338"\
       # -ex "b diff.c:free_comparison_mem"\
       # -ex "b diff.c:try_possible_paths"\
 else
-  ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
-  # ./nvim -u NONE -d ../../testdiffb.txt ../../testdiffa.txt
+  # ./nvim -u NONE -d ../../testdiff3_1.txt ../../testdiff3_2.txt ../../testdiff3_3.txt
+  ./nvim -u NONE -d ../../testdiffb.txt ../../testdiffa.txt
 
 fi
 # gdb \
---
+++ gdbscript.gdb
@@ -0,0 +1,3 @@
+display {*topdiff->df_lnum@3, *topdiff->df_count@3}
+display {*localtopdiff->df_lnum@3, *localtopdiff->df_count@3}
+display {*thistopdiff->df_lnum@3, *thistopdiff->df_count@3}
