e3588279b642b93fd19f431cda5416149040272d
jonathon <jonathonwhite@protonmail.com>
2022-03-26 11:08:33 -0400

    add the 'b'

--- bin/gdbsetbreakpoints.sh
+++ bin/gdbsetbreakpoints.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 grep -r -n --include \*.h --include \*.cpp --include \*.c "GDBBREAKPOINT" .\
-	| cut -d ' ' -f 1 | rev | cut -d '/' -f 1 | rev | sed 's/.$//' > breakpoints.gdb
+	| cut -d ' ' -f 1 | rev | cut -d '/' -f 1 | rev | sed 's/.$//' | sed "s/^/b /" > breakpoints.gdb
 cat breakpoints.gdb
