9ad6701bf879637b3fc3f8a6260b47bca6678246
Jonathon <jonathonwhite@protonmail.com>
2022-04-04 17:54:57 -0400

    rename diffcomparisonpath_flat

--- src/nvim/linematch.h
+++ src/nvim/linematch.h
@@ -6,7 +6,7 @@ typedef struct {
   int *df_decision;  // to keep track of this path traveled
   int df_lev_score;  // to keep track of the total score of this path
   int df_path_index;  // current index of this path
-} diffcomparisonpath_flat_T;
+} diffcomparisonpath_T;
 
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "linematch.h.generated.h"
